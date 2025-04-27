6fcf444d3a7a3a5875ece4093e622b6c5ea5bb2a
JakubKonert <kubowski2000@gmail.com>
2025-04-27 20:47:00 +0200

    Initial Commit

---
+++ StockIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockIOS
+//
+//  Created by student on 27/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
